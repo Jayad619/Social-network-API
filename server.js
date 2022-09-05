21d3815575d4756672c39a886335dccd211e0d6d
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-06 00:14:28 +0100

    connected to server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/connection");
 
-const cwd = process.cwd();
+// const routes = require('./routes');
 
 const PORT = process.env.port || 3001;
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
