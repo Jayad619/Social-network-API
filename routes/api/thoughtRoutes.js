5e0f5fa69aedc8571a1c000e0f12260890652885
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-07 18:22:49 +0100

    completed thought controller func

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,32 +3,36 @@ const router = require('express').Router();
 const {
   getAllThoughts,
   getThoughtById,
-  createNewThought,
-  deleteThought
+  createThought,
+  deleteThought,
+  updateThought,
+  createReaction,
+  deleteReaction
 } = require('../../controllers/thoughtController');
 
 // just get all the thoughts. Get & create
 router
 .route('/')
  .get(getAllThoughts)
- .post(createNewThought)
+ .post(createThought)
 
 // get a thought by its id. Update & delete
 router
 .route('/:id')
 .get(getThoughtById)
 .delete(deleteThought)
-
+.put(updateThought)
 
 
 
 // create a new reaction per thought using its id
 router
 .route('/:thoughtId/reactions')
+.post(createReaction)
 
 // delete a reaction to A THOUGHT using its ID
 router
 .route('/:thoughtId/reactions/:reactionId')
-
+.delete(deleteReaction)
 
 module.exports = router;
